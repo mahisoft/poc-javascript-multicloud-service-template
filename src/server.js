b830792be482c6a879a72819ce96260cda7fd48d
davidbouu <dbou@mahisoft.com>
2021-11-19 19:53:51 -0400

    test

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ console.log("=------------------==")
 const cssLinksFromAssets = (assets, entrypoint) => {
   return assets[entrypoint] ? assets[entrypoint].css ?
   assets[entrypoint].css.map(asset=>
-    `<link rel="stylesheet" href="dev${asset}">`
+    `<link rel="stylesheet" href="${asset}">`
   ).join('') : '' : '';
 };
 
