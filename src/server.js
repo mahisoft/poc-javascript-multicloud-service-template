70fc2423e055520d6a53524f1b231f9af2a26624
davidbouu <dbou@mahisoft.com>
2021-11-19 19:45:05 -0400

    test

--- src/server.js
+++ src/server.js
@@ -14,14 +14,14 @@ console.log("=------------------==")
 const cssLinksFromAssets = (assets, entrypoint) => {
   return assets[entrypoint] ? assets[entrypoint].css ?
   assets[entrypoint].css.map(asset=>
-    `<link rel="stylesheet" href="dev/${asset}">`
+    `<link rel="stylesheet" href="${asset}">`
   ).join('') : '' : '';
 };
 
 const jsScriptTagsFromAssets = (assets, entrypoint, extra = '') => {
   return assets[entrypoint] ? assets[entrypoint].js ?
   assets[entrypoint].js.map(asset=>
-    `<script src="dev/${asset}"${extra}></script>`
+    `<script src="${asset}"${extra}></script>`
   ).join('') : '' : '';
 };
 
