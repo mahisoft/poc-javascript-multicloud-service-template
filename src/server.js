1066c6a1b8df9675f526b03880fceb1498ddaaa4
davidbouu <dbou@mahisoft.com>
2021-11-19 20:08:33 -0400

    test

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ console.log(process.env.RAZZLE_ASSETS_MANIFEST)
 console.log("=------------------==")
 console.log(assets)
 console.log("=------------------==")
-let dev=""
+let dev="https://p9a6mz281c.execute-api.us-east-1.amazonaws.com/dev"
 
 const cssLinksFromAssets = (assets, entrypoint) => {
   return assets[entrypoint] ? assets[entrypoint].css ?
